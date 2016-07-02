4342a33bb141d7bc45fbe5bd6e0d389e06048618
momentspace <service@starferry.net>
2016-07-02 11:21:14 +0900

    add tutorial code

---
+++ index.js
@@ -0,0 +1,16 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var CommentBox = require('./app')
+
+
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+]
+
+ReactDOM.render(
+  <CommentBox url="http://localhost:3000/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+)
+
